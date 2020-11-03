1c2309390118f896f62f4febb99d3feb65aa7542
dedaniyaprayag <dedaniyaprayag@gmail.com>
2020-11-03 15:28:18 -0500

    Added RabbitMq Configuration

---
+++ SiteAvailabilityProcessor/Config/IRabbitMqConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace SiteAvailabilityProcessor.Config
+{
+    public interface IRabbitMqConfiguration
+    {
+        string Hostname { get; set; }
+
+        string QueueName { get; set; }
+
+        string UserName { get; set; }
+
+        string Password { get; set; }
+        int Port { get; set; }
+    }
+}
